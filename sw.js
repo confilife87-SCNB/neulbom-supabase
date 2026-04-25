5862a5cee2c26cc2e893e9e4f458dd0ebc4395a2
confilife87-SCNB <confilife87@gmail.com>
2026-04-25 18:08:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+var CACHE = 'neulbom-supabase-v1';
 
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll([
+        '/neulbom-supabase/',
+        '/neulbom-supabase/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
